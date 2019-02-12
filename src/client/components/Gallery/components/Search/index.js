8578c28db9467b714877b62b86763f950a428ce0
Denis Novikov <den-vse@yandex.ru>
2019-02-13 00:57:47 +0300

    chore: reorganize search input view

--- src/client/components/Gallery/components/Search/index.js
+++ src/client/components/Gallery/components/Search/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { ArrowRightRounded } from '@material-ui/icons'
+import { TextField } from '@material-ui/core'
 
 import './index.css'
 
 export default ({ query, change }) => (
-  <div className="search">
-    <ArrowRightRounded className="arrow"/>
-    <input
-      className="input"
-      type="text"
-      value={query}
-      onChange={(e) => change(e.target.value)}
-      placeholder="Поиск"
-    />
-  </div>
+  <TextField
+    className="search"
+    label="Поиск"
+    helperText="Введите поисковый запрос"
+    value={query}
+    onChange={(e) => change(e.target.value)}
+    margin="none"
+    variant="filled"
+    fullWidth={true}
+  />
 )
