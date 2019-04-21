d614149d0d36dceb4ba80dac4490a5026cf70964
Denis Novikov <den-vse@yandex.ru>
2019-04-21 20:50:03 +0300

    refactor: move description eater to method

--- src/client/components/Gallery/Viewer/Explanation/index.js
+++ src/client/components/Gallery/Viewer/Explanation/index.js
@@ -3,83 +3,83 @@ import React, { PureComponent, Fragment } from 'react';
 import Block from '../Block';
 
 export default class Explanation extends PureComponent {
+
+  eatDescription = ({description='', jsoned}, params) => {
+    console.log('eatDescription');
+    console.log(description);
+    const dotsIndex = description.lastIndexOf(':');
+    if (description && dotsIndex !== -1) {
+      let value;
+      const foundCurlesIds = [description.lastIndexOf('{'), description.lastIndexOf('}')];
+      console.log('foundCurlesIds');
+      console.log(foundCurlesIds);
+
+      let commed = false;
+      let commaIndex;
+      let leftDescription;
+
+      if (foundCurlesIds[0] !== -1 && foundCurlesIds[1] !== -1 && (description.length-1) === foundCurlesIds[1]) {
+        commed = true;
+        console.log('commed');
+        console.log(commed);
+        console.log(description[foundCurlesIds[0]]);
+        console.log(description[foundCurlesIds[1]]);
+        value = description.substring(foundCurlesIds[0]+1, foundCurlesIds[1]);
+        console.log('value');
+        console.log(value);
+
+        leftDescription = description.substring(0, foundCurlesIds[0]+1);
+        console.log('leftDescription1');
+        console.log(leftDescription);
+        leftDescription = leftDescription.substring(0, leftDescription.lastIndexOf(':')).trim();
+        console.log('leftDescription2');
+        console.log(leftDescription);
+
+        commaIndex = leftDescription.lastIndexOf(',');
+      } else {
+        value = description.substring(dotsIndex+1, description.length+1).trim();
+
+        leftDescription = description.substring(0, dotsIndex);
+        commaIndex = leftDescription.lastIndexOf(',');
+      }
+
+      console.log('value');
+      console.log(value);
+
+      console.log('leftDescription');
+      console.log(leftDescription);
+
+      if (commaIndex !== -1) {
+        const param = leftDescription.substring(commaIndex+1, leftDescription.length).trim();
+        console.log('param');
+        console.log(param);
+        params.unshift([param, value, jsoned, commed]);
+        this.eatDescription({description: leftDescription.substring(0, commaIndex), jsoned}, params);
+      } else {
+        console.log('leftDescription');
+        console.log(leftDescription);
+        params.unshift([leftDescription, value, jsoned, commed]);
+      }
+    }
+
+    return params;
+  };
+
   render() {
     const { explanation } = this.props;
     const { details=[] } = explanation;
 
     let { description } = explanation;
-    console.log('description');
-    console.log(description);
 
     const jsoned = description[0] === '{' && description[description.length-1] === '}';
     if (jsoned) {
       description = description.substring(1, description.length-1);
-      console.log('JSONED');
-      console.log(description);
     }
 
-    const eatDescription = (description, params) => {
-      console.log('eatDescription');
-      console.log(description);
-      const dotsIndex = description.lastIndexOf(':');
-      if (description && dotsIndex !== -1) {
-        let value;
-        const foundCurlesIds = [description.lastIndexOf('{'), description.lastIndexOf('}')];
-        console.log('foundCurlesIds');
-        console.log(foundCurlesIds);
-
-        let commed = false;
-        let commaIndex;
-        let leftDescription;
-
-        if (foundCurlesIds[0] !== -1 && foundCurlesIds[1] !== -1 && (description.length-1) === foundCurlesIds[1]) {
-          commed = true;
-          console.log('commed');
-          console.log(commed);
-          console.log(description[foundCurlesIds[0]]);
-          console.log(description[foundCurlesIds[1]]);
-          value = description.substring(foundCurlesIds[0]+1, foundCurlesIds[1]);
-          console.log('value');
-          console.log(value);
-
-          leftDescription = description.substring(0, foundCurlesIds[0]+1);
-          console.log('leftDescription1');
-          console.log(leftDescription);
-          leftDescription = leftDescription.substring(0, leftDescription.lastIndexOf(':')).trim();
-          console.log('leftDescription2');
-          console.log(leftDescription);
-
-          commaIndex = leftDescription.lastIndexOf(',');
-        } else {
-          value = description.substring(dotsIndex+1, description.length+1).trim();
-
-          leftDescription = description.substring(0, dotsIndex);
-          commaIndex = leftDescription.lastIndexOf(',');
-        }
-
-        console.log('value');
-        console.log(value);
-
-        console.log('leftDescription');
-        console.log(leftDescription);
-
-        if (commaIndex !== -1) {
-          const param = leftDescription.substring(commaIndex+1, leftDescription.length).trim();
-          console.log('param');
-          console.log(param);
-          params.unshift([param, value, jsoned, commed]);
-          eatDescription(leftDescription.substring(0, commaIndex), params);
-        } else {
-          console.log('leftDescription');
-          console.log(leftDescription);
-          params.unshift([leftDescription, value, jsoned, commed]);
-        }
-      }
-
-      return params;
-    };
-
-    const params = eatDescription(description, []);
+    console.log(description);
+    const params = this.eatDescription({ description, jsoned }, []);
+    console.log('params are');
+    console.log(params);
 
     const blockContent = params.map(([param, value, jsoned, commed], idx) => {
       if (commed) {
@@ -89,7 +89,7 @@ export default class Explanation extends PureComponent {
           </Block>
           <Block>
             {
-              eatDescription(value, []).map(([param, value], i) => (
+              this.eatDescription({description: value, jsoned}, []).map(([param, value], i) => (
                 <div key={i+param}>{param}: {value}</div>
               ))
             }
