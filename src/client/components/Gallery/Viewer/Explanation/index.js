76e8de15a96b78e163c7344aa15aa8288d8ec243
Denis Novikov <den-vse@yandex.ru>
2019-04-21 21:34:47 +0300

    enhancement: show param name of commed param in same string

--- src/client/components/Gallery/Viewer/Explanation/index.js
+++ src/client/components/Gallery/Viewer/Explanation/index.js
@@ -2,17 +2,19 @@ import React, { PureComponent, Fragment } from 'react';
 
 import Block from '../Block';
 
+import styles from './index.css'
+
 export default class Explanation extends PureComponent {
 
   eatDescription = ({description='', jsoned}, params) => {
-    console.log('eatDescription');
-    console.log(description);
+    // console.log('eatDescription');
+    // console.log(description);
     const dotsIndex = description.lastIndexOf(':');
     if (description && dotsIndex !== -1) {
       let value;
       const foundCurlesIds = [description.lastIndexOf('{'), description.lastIndexOf('}')];
-      console.log('foundCurlesIds');
-      console.log(foundCurlesIds);
+      // console.log('foundCurlesIds');
+      // console.log(foundCurlesIds);
 
       let commed = false;
       let commaIndex;
@@ -20,20 +22,20 @@ export default class Explanation extends PureComponent {
 
       if (foundCurlesIds[0] !== -1 && foundCurlesIds[1] !== -1 && (description.length-1) === foundCurlesIds[1]) {
         commed = true;
-        console.log('commed');
-        console.log(commed);
-        console.log(description[foundCurlesIds[0]]);
-        console.log(description[foundCurlesIds[1]]);
+        // console.log('commed');
+        // console.log(commed);
+        // console.log(description[foundCurlesIds[0]]);
+        // console.log(description[foundCurlesIds[1]]);
         value = description.substring(foundCurlesIds[0]+1, foundCurlesIds[1]);
-        console.log('value');
-        console.log(value);
+        // console.log('value');
+        // console.log(value);
 
         leftDescription = description.substring(0, foundCurlesIds[0]+1);
-        console.log('leftDescription1');
-        console.log(leftDescription);
+        // console.log('leftDescription1');
+        // console.log(leftDescription);
         leftDescription = leftDescription.substring(0, leftDescription.lastIndexOf(':')).trim();
-        console.log('leftDescription2');
-        console.log(leftDescription);
+        // console.log('leftDescription2');
+        // console.log(leftDescription);
 
         commaIndex = leftDescription.lastIndexOf(',');
       } else {
@@ -43,21 +45,21 @@ export default class Explanation extends PureComponent {
         commaIndex = leftDescription.lastIndexOf(',');
       }
 
-      console.log('value');
-      console.log(value);
+      // console.log('value');
+      // console.log(value);
 
-      console.log('leftDescription');
-      console.log(leftDescription);
+      // console.log('leftDescription');
+      // console.log(leftDescription);
 
       if (commaIndex !== -1) {
         const param = leftDescription.substring(commaIndex+1, leftDescription.length).trim();
-        console.log('param');
-        console.log(param);
+        // console.log('param');
+        // console.log(param);
         params.unshift([param, value, jsoned, commed]);
         this.eatDescription({description: leftDescription.substring(0, commaIndex), jsoned}, params);
       } else {
-        console.log('leftDescription');
-        console.log(leftDescription);
+        // console.log('leftDescription');
+        // console.log(leftDescription);
         params.unshift([leftDescription, value, jsoned, commed]);
       }
     }
@@ -76,7 +78,7 @@ export default class Explanation extends PureComponent {
       description = description.substring(1, description.length-1);
     }
 
-    console.log(description);
+    // console.log(description);
     const params = this.eatDescription({ description, jsoned }, []);
     console.log('params are');
     console.log(params);
@@ -85,12 +87,14 @@ export default class Explanation extends PureComponent {
       if (commed) {
         return <Fragment key={param+idx+'_param_commed'} >
           <Block uncollapsable={true}>
-            {param}
-          </Block>
-          <Block>
+            <span className={styles.param}>{param}: </span>
             {
-              this.eatDescription({description: value, jsoned}, []).map(([param, value], i) => (
-                <div key={i+param}>{param}: {value}</div>
+              this.eatDescription({ description: value, jsoned }, []).map(([param, value], i, arr) => (
+                <span key={i+param}>
+                  <span className={styles.subParam}>{param}:</span>
+                  <span>{value}</span>
+                  { arr.length !== i+1 && <span>, </span> }
+                </span>
               ))
             }
           </Block>
